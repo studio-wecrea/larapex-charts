96ee121dfa3219087fb31e099c40d2b8aaac5c10
Dorian NUNEZ <dorian@wecrea.com>
2023-05-11 11:37:36 +0200

    Try understand library 26

--- stubs/resources/views/chart/script.blade.php
+++ stubs/resources/views/chart/script.blade.php
@@ -29,6 +29,15 @@
         },
         xaxis: {
             categories: {!! $chart->xAxis() !!},
+            ticks:{
+                callback:(value,index,values)=>{
+                    if (value.split(":")[1] === "00") {
+                    return value; // Afficher le label
+                    } else {
+                    return ''; // Ne pas afficher le label
+                    }
+                }
+            },
             @if($chart->tickAmount())
             tickAmount: {!! $chart->tickAmount() !!},
             @endif
