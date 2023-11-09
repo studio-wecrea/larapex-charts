60f06908e7e0761c479b813199d30ef68db2c54e
Dorian NUNEZ <dorian@wecrea.com>
2023-11-09 02:50:20 +0100

    Test char

--- src/Traits/ComplexChartDataAggregator.php
+++ src/Traits/ComplexChartDataAggregator.php
@@ -4,14 +4,13 @@ namespace ArielMejiaDev\LarapexCharts\Traits;
 
 trait ComplexChartDataAggregator
 {
-    public function addData(string $name, array $data,string $type) 
+    public function addData(string $name, array $data) 
     {
         $this->dataset = json_decode($this->dataset);
 
         $this->dataset[] = [
             'name' => $name,
             'data' => $data,
-            'type' => $type,
         ];
 
         $this->dataset = json_encode($this->dataset);
